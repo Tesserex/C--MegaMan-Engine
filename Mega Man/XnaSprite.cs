5a708625cd3c9b5c407a914f9f980261af1238bc
Tesserex <tesserex@gmail.com>
2012-07-26 19:32:29 -0500

    DOESNT COMPILE

---
+++ Mega Man/XnaSprite.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MegaMan.Common;
+
+using DrawPoint = System.Drawing.Point;
+using DrawRectangle = System.Drawing.Rectangle;
+
+using XnaRectangle = Microsoft.Xna.Framework.Rectangle;
+using XnaColor = Microsoft.Xna.Framework.Color;
+
+namespace MegaMan.Engine
+{
+    public class XnaSprite : Sprite
+    {
+        private List<Texture2D> _paletteSwaps;
+    }
+}
