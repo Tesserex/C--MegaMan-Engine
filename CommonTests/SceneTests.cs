b7795d480fc2ab1a64f6f882d663c67a2767ac15
Tesserex <tesserex@gmail.com>
2013-04-08 22:02:01 -0500

    add scene test file

---
+++ CommonTests/SceneTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MegaMan.Common.Tests
+{
+    [TestClass]
+    public class SceneTests
+    {
+        [TestMethod, TestCategory("Scene")]
+        public void LoadScene()
+        {
+            var xmlString = TestHelpers.GetInputFile("Scene_Correct.xml");
+            var xml = XElement.Parse(xmlString);
+            var scene = SceneInfo.FromXml(xml, @"C:\");
+
+            Assert.AreEqual("TestScene", scene.Name);
+        }
+    }
+}
