354f62e6296c130b8901a0329a8e3d9f66d50ddd
Craig <craig@liquidfyre.com>
2013-11-23 10:23:33 -0600

    got ribbon, wip on new project dialog

---
+++ WPFEditor/Controls/NewProjectDialog.xaml.cs
@@ -0,0 +1,54 @@
+﻿using MegaMan.Editor.Controls.ViewModels;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MegaMan.Editor.Controls
+{
+    /// <summary>
+    /// Interaction logic for NewProjectDialog.xaml
+    /// </summary>
+    public partial class NewProjectDialog : UserControl
+    {
+        private NewProjectViewModel _viewModel;
+
+        public NewProjectDialog()
+        {
+            InitializeComponent();
+
+            _viewModel = new NewProjectViewModel();
+            this.DataContext = _viewModel;
+        }
+
+        private void BrowseClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            dialog.InitialDirectory = _viewModel.DirectoryPath;
+            dialog.Title = "Choose Project Location";
+            dialog.EnsureFileExists = true;
+            dialog.EnsurePathExists = true;
+            dialog.EnsureReadOnly = false;
+            dialog.EnsureValidNames = true;
+            dialog.Multiselect = false;
+            dialog.ShowPlacesList = true;
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                _viewModel.DirectoryPath = dialog.FileName;
+            }
+        }
+    }
+}
---
+++ WPFEditor/Controls/ViewModels/NewProjectViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaMan.Editor.Controls.ViewModels
+{
+    public class NewProjectViewModel : INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        private string _author;
+        public string Author
+        {
+            get { return _author; }
+            set
+            {
+                _author = value;
+                OnPropertyChanged("Author");
+            }
+        }
+
+        private string _directoryPath;
+        public string DirectoryPath
+        {
+            get { return _directoryPath; }
+            set
+            {
+                _directoryPath = value;
+                OnPropertyChanged("DirectoryPath");
+            }
+        }
+
+        public bool CreateProjectDirectory { get; set; }
+
+        public NewProjectViewModel()
+        {
+            Name = GetDefaultProjectName();
+            Author = GetMostRecentAuthor();
+            DirectoryPath = GetMostRecentDirectory();
+            CreateProjectDirectory = true;
+        }
+
+        private string GetMostRecentDirectory()
+        {
+            return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        }
+
+        private string GetMostRecentAuthor()
+        {
+            return "";
+        }
+
+        private string GetDefaultProjectName()
+        {
+            return "My Fan Game";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string property)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
