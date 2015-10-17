a58b8ab981c718013c106cd31c222a7ea2b08438
Tesserex <tesserex@gmail.com>
2015-10-17 16:32:22 -0700

    convert to angular spa

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'pages/home.html'
+		})
+		.when('/download', {
+			templateUrl: 'pages/download.html'
+		})
+		.when('/features', {
+			templateUrl: 'pages/features.html'
+		})
+		.when('/docs', {
+			templateUrl: 'pages/docs.html'
+		})
+		.when('/resources', {
+			templateUrl: 'pages/resources.html'
+		});
+})
+app.controller('mainController', ['$scope', function($scope) {
+
+}]);
