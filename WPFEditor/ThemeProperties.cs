88b534116f0cc13c177449671b73523e80864def
Tesserex <tesserex@gmail.com>
2015-10-23 22:16:19 -0700

    update button styles and make them consistent

---
+++ WPFEditor/ThemeProperties.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MegaMan.Editor
+{
+    public static class ThemeProperties
+    {
+        public static CornerRadius GetButtonRadius(DependencyObject obj)
+        {
+            return (CornerRadius)obj.GetValue(ButtonRadiusProperty);
+        }
+
+        public static void SetButtonRadius(DependencyObject obj, CornerRadius value)
+        {
+            obj.SetValue(ButtonRadiusProperty, value);
+        }
+
+        public static readonly DependencyProperty ButtonRadiusProperty =
+        DependencyProperty.RegisterAttached(
+            "ButtonRadius",
+            typeof(CornerRadius),
+            typeof(ThemeProperties),
+            new FrameworkPropertyMetadata(new CornerRadius(4)));
+    }
+}
