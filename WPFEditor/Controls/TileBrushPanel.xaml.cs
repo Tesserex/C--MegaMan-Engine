973ae48da2160a997c0315a84f04242c8c62e82b
Tesserex <tesserex@gmail.com>
2016-03-27 22:46:26 -0700

    change tilestrip in tileset editor to tile panel

---
+++ WPFEditor/Controls/TileBrushPanel.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using MegaMan.Editor.Bll.Tools;
+using MegaMan.Editor.Controls.ViewModels;
+
+namespace MegaMan.Editor.Controls
+{
+    /// <summary>
+    /// Interaction logic for TilePanelControl.xaml
+    /// </summary>
+    public partial class TileBrushPanel : UserControl
+    {
+        public static readonly RoutedUICommand clickBrushCommand = new RoutedUICommand("BrushClick", "BrushClick", typeof(TileBrushPanel));
+
+        public TileBrushPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void BrushClick(object sender, ExecutedRoutedEventArgs e)
+        {
+            ((TilePanelControlViewModel)this.DataContext).SelectBrush((MultiTileBrush)e.Parameter);
+        }
+    }
+}
---
+++ WPFEditor/Controls/TilePanel.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using MegaMan.Common;
+using MegaMan.Editor.Controls.ViewModels;
+
+namespace MegaMan.Editor.Controls
+{
+    /// <summary>
+    /// Interaction logic for TilePanel.xaml
+    /// </summary>
+    public partial class TilePanel : UserControl
+    {
+        public TilePanel()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly RoutedUICommand clickTileCommand = new RoutedUICommand("TileClick", "TileClick", typeof(TilePanel));
+
+        private void TileClick(object sender, ExecutedRoutedEventArgs e)
+        {
+            ((TilesetViewModelBase)this.DataContext).ChangeTile((Tile)e.Parameter);
+        }
+    }
+}
