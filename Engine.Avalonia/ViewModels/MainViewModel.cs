29c3658b077161d78942d210681e89bdb18910ee
Tesserex <tesserex@gmail.com>
2023-10-26 17:10:15 -0500

    Some temporary input handling

--- Engine.Avalonia/ViewModels/MainViewModel.cs
+++ Engine.Avalonia/ViewModels/MainViewModel.cs
@@ -1,8 +1,12 @@
-﻿using Avalonia;
+﻿using System.Collections.Generic;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Threading;
 using MegaMan.Engine;
 using MegaMan.Engine.Avalonia;
+using MegaMan.Engine.Input;
+using Microsoft.Xna.Framework.Input;
 
 namespace Engine.Avalonia.ViewModels;
 
@@ -49,6 +53,8 @@ public class MainViewModel : ViewModelBase
     public string ThinkLabel { get => thinkLabel ?? ""; set { thinkLabel = value; OnPropertyChanged(); } }
     public string EntityLabel { get => entityLabel ?? ""; set { SetProperty(ref entityLabel, value); } }
 
+    private Dictionary<Key, bool> keysPressed = new Dictionary<Key, bool>();
+
     public MainViewModel()
     {
         if (!Design.IsDesignMode)
@@ -64,6 +70,27 @@ public class MainViewModel : ViewModelBase
         DefaultScreen();
 
         MegaMan.Engine.Engine.Instance.GameLogicTick += Instance_GameLogicTick;
+
+        InputElement.KeyDownEvent.AddClassHandler<TopLevel>(OnKeyDown, handledEventsToo: true);
+        InputElement.KeyUpEvent.AddClassHandler<TopLevel>(OnKeyUp, handledEventsToo: true);
+
+        GameInput.AddBinding(new KeyboardInputBinding(GameInputs.Start, Keys.Enter, k => keysPressed.GetValueOrDefault(Key.Enter)));
+        GameInput.AddBinding(new KeyboardInputBinding(GameInputs.Left, Keys.Left, k => keysPressed.GetValueOrDefault(Key.Left)));
+        GameInput.AddBinding(new KeyboardInputBinding(GameInputs.Up, Keys.Up, k => keysPressed.GetValueOrDefault(Key.Up)));
+        GameInput.AddBinding(new KeyboardInputBinding(GameInputs.Down, Keys.Down, k => keysPressed.GetValueOrDefault(Key.Down)));
+        GameInput.AddBinding(new KeyboardInputBinding(GameInputs.Right, Keys.Right, k => keysPressed.GetValueOrDefault(Key.Right)));
+        GameInput.AddBinding(new KeyboardInputBinding(GameInputs.Jump, Keys.A, k => keysPressed.GetValueOrDefault(Key.A)));
+        GameInput.AddBinding(new KeyboardInputBinding(GameInputs.Shoot, Keys.O, k => keysPressed.GetValueOrDefault(Key.O)));
+    }
+
+    private void OnKeyDown(object s, KeyEventArgs e)
+    {
+        keysPressed[e.Key] = true;
+    }
+
+    private void OnKeyUp(object s, KeyEventArgs e)
+    {
+        keysPressed[e.Key] = false;
     }
 
     private void DefaultScreen()
