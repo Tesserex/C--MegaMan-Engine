8652e96811ff9c3c24fa73c82d7024b4e1cf9553
Tesserex <tesserex@gmail.com>
2024-01-04 16:55:21 -0600

    style the main menu

---
+++ Engine.Avalonia/Controls/MenuHeader.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+
+namespace MegaMan.Engine.Avalonia;
+public class MenuHeader : TemplatedControl
+{
+    public static StyledProperty<string?> LabelProperty = AvaloniaProperty.Register<MenuHeader, string?>(nameof(Label));
+
+    public string? Label
+    {
+        get { return GetValue(LabelProperty); }
+        set { SetValue(LabelProperty, value); }
+    }
+
+    public static StyledProperty<string?> HotKeyProperty = AvaloniaProperty.Register<MenuHeader, string?>(nameof(HotKey));
+
+    public string? HotKey
+    {
+        get { return GetValue(HotKeyProperty); }
+        set { SetValue(HotKeyProperty, value); }
+    }
+}
